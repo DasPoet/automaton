f9c00e473b1f98f10d265ea784403ad7bd38b630
Daspoet <cederk2306@gmail.com>
2020-09-09 20:48:17 +0200

    Make fields lowercase

--- src/main/java/dev/daspoet/Pair.java
+++ src/main/java/dev/daspoet/Pair.java
@@ -5,12 +5,12 @@ import lombok.Getter;
 @Getter
 public class Pair<F, S> {
 
-    private final F First; // first entry
-    private final S Second; // second entry
+    private final F first; // first entry
+    private final S second; // second entry
 
     // Pair constructs a new Pair from an arbitrary pair of values and returns it.
     public Pair(F first, S second) {
-        First = first;
-        Second = second;
+        this.first = first;
+        this.second = second;
     }
 }
